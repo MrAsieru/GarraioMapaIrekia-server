e028e1eb3ef5cf93adad3453834833afd02cfa7e
MrAsieru <28875845+MrAsieru@users.noreply.github.com>
2023-09-25 12:29:56 +0200

    fix: mover archivos mongodb y añadir rol dbAdmin

--- init-mongo.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-mongosh <<EOF
-use  ${MONGODB_INITDB_DATABASE}
-db.createUser({user: "${MONGODB_SERVER_USER}", pwd: "${MONGODB_SERVER_USER_PASSWORD}", roles: [{role: 'readWrite', db: "${MONGODB_INITDB_DATABASE}"}]})
-db.createUser({user: "${MONGODB_API_USER}", pwd: "${MONGODB_API_USER_PASSWORD}", roles: [{role: 'read', db: "${MONGODB_INITDB_DATABASE}"}]})
-EOF
---
+++ mongodb/init-mongo.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+mongosh <<EOF
+use  ${MONGODB_INITDB_DATABASE}
+db.createUser({user: "${MONGODB_SERVER_USER}", pwd: "${MONGODB_SERVER_USER_PASSWORD}", roles: [{role: 'readWrite', db: "${MONGODB_INITDB_DATABASE}"}, {role: 'dbAdmin', db: "${MONGODB_INITDB_DATABASE}"}]})
+db.createUser({user: "${MONGODB_API_USER}", pwd: "${MONGODB_API_USER_PASSWORD}", roles: [{role: 'read', db: "${MONGODB_INITDB_DATABASE}"}]})
+EOF
