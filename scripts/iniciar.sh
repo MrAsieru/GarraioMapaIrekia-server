ec26a955b9deaf6b0e00dbe18f2b406fc3b6cf4b
MrAsieru <28875845+MrAsieru@users.noreply.github.com>
2023-12-13 16:29:41 +0100

    chore: mover archivos

--- scripts/iniciar.sh
+++ scripts/iniciar.sh
@@ -2,20 +2,17 @@
 # Establecer variables de entorno
 source ./mongodb/mongodb.env
 
-# Iniciar servidor
-docker compose up -d --build
-
 # Descargar datos GTFS
-python3 ./obtener_gtfs.py
+python ./obtener_gtfs.py
 
 # Insertar datos en MongoDB
-python3 ./gtfs_to_mongodb.py
+python ./gtfs_to_mongodb.py
 
 # Generar archivos GeoJSON
-python3 ./gtfs_to_geojson.py
+python ./gtfs_to_geojson.py
 
 # Generar archivos PMTiles y reiniciar servidor Martin
 sh ./generar_tiles.sh
 
 # Calcular posiciones
-python3 ./calcular_posiciones.py
+python ./calcular_posiciones.py
